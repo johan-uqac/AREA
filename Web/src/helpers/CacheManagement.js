d82b9e990da85f180faa8d748697b4be2d0174f0
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-23 15:41:03 -0500

    Add auth with cache

---
+++ Web/src/helpers/CacheManagement.js
@@ -0,0 +1,24 @@
+export function getDataFromCache(cacheName) {
+    try {
+        const data = localStorage.getItem(cacheName);
+        if (data) {
+            var newData = JSON.parse(data);
+            newData.password = atob(newData.password)
+            return newData;
+        } else {
+            console.log("no data in cache");
+            return null;
+        }
+    } catch (error) {
+        console.log("error getting cache: " + error);
+    }
+}
+
+export function addDataIntoCache(cacheName, response) {
+    try {
+        console.log("addDataIntoCache: " + JSON.stringify(response));
+        localStorage.setItem(cacheName, JSON.stringify(response))
+    } catch (error) {
+        console.log("error adding cache: " + error);
+    }
+}
