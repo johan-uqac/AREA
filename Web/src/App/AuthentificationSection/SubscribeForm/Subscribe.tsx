89c8f5458d36923879ed543ac8ee30a69f08dc9f
gaby <gabrielle.walgraef@epitech.eu>
2023-11-28 14:45:22 -0500

    register design

--- Web/src/App/AuthentificationSection/SubscribeForm/Subscribe.tsx
+++ Web/src/App/AuthentificationSection/SubscribeForm/Subscribe.tsx
@@ -1,5 +1,5 @@
 
-import pic from '../../Components/assets/pic.png'
+import pic from '../../Components/assets/pic-register.jpg'
 import backgroundImage from '../../Components/assets/background.jpg';
 
 var sectionStyle = {
@@ -8,6 +8,36 @@ var sectionStyle = {
 
 export default function SubscribeForm({ register, handleSubmit, errors, onSubmit }: any) {
   return (
+    <div className="bg-image" style={sectionStyle}>
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+      <div className="md:w-1/3 max-w-sm">
+        <img
+          src={pic}
+          alt="Sample image" />
+      </div>
+      <div className="md:w-1/3 max-w-sm">
+        <div className="text-center md:text-center">
+          <label className="mr-1 font-bold text-2xl">Register</label>
+        </div>
+        <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+        <label className=""> Join us ! </label>
+        
+        </div>
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder="Create a Pseudo" />
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Email Address" />
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" />
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Confirm password" />
+        <div className="text-center md:text-center">
+        <button class="mb-2 mt-2 w-[150px] bg-black h-[50px] rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-teal-600 before:to-[rgb(13,148,136)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-slate-50" type="submit">
+            Register
+        </button>
+        </div>
+        <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
+         Already have an account? <a className="text-teal-600 hover:underline hover:underline-offset-4" href="#">Sign in</a>
+        </div>
+      </div>
+    </section>
+      </div>
     
   )
 }
